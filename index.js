6f19e1def7de6ab4a0c467085177877c30490ef8
Rahul Pate <rahul.p@simformsolutions.com>
2022-04-25 18:50:59 +0530

    intial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+var express = require("express");
+var app = express();
+app.get('/hello',function(req,res){
+        //Prints all the headers and its value as JavaScript object.
+        console.log(JSON.stringify(req.headers));
+        //Print the value of header 'first_name'.
+        res.send(JSON.stringify(req.headers));
+});
+
+
+app.listen(8081, function () {
+    console.log("Headers app Running on port 8081");
+});
